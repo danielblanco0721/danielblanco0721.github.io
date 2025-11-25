15c95ea231ce36b4851f50403bea773cfcceee67
danielblanco0721 <danielblanforero@gmail.com>
2025-11-25 13:39:53 -0500

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https//danielblanco0721.github.io',
   vite: {
     plugins: [tailwindcss()],
   },
