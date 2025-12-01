28ec1b4caa737dbd203df9631dec3403ac534d7c
danielblanco0721 <danielblanforero@gmail.com>
2025-11-30 20:27:37 -0500

    links

--- src/components/commons/socialIcons/socialIcons.jsx
+++ src/components/commons/socialIcons/socialIcons.jsx
@@ -9,7 +9,7 @@ const SocialIcons = () => {
   return (
     <div className="my-5 flex items-center justify-center">
       <a
-        href="https://www.linkedin.com/in/lautaro-rodr%C3%ADguez-collins-40505624a/"
+        href="https://www.linkedin.com/in/danes17010/"
         target="_blank"
         rel="noopener noreferrer"
         className="mr-3 text-zinc-400 hover:text-[#69c7c7] drop-shadow-[1px_1px_0_#7836cf]"
@@ -19,7 +19,7 @@ const SocialIcons = () => {
       </a>
 
       <a
-        href="https://github.com/Lautaro-R-collins"
+        href="https://www.linkedin.com/in/danes17010/"
         target="_blank"
         rel="noopener noreferrer"
         className="mx-3 text-zinc-400 hover:text-[#69c7c7] drop-shadow-[1px_1px_0_#7836cf]"
