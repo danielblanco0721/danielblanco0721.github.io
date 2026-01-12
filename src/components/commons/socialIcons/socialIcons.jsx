8f5dd41ca1acd54f660fe7cb18d0337a0879d378
danielblanco0721 <danielblanforero@gmail.com>
2026-01-12 14:24:03 -0500

    links

--- src/components/commons/socialIcons/socialIcons.jsx
+++ src/components/commons/socialIcons/socialIcons.jsx
@@ -1,51 +1,26 @@
-import {
-  FaXTwitter,
-  FaLinkedinIn,
-  FaGithub,
-  FaInstagram,
-} from "react-icons/fa6";
+import { FaLinkedinIn, FaGithub } from "react-icons/fa6";
 
 const SocialIcons = () => {
   return (
-    <div className="my-5 flex items-center justify-center">
+    <div className="my-5 flex items-center justify-center gap-6">
       <a
-        href="https://www.linkedin.com/in/danes17010/"
+        href="https://www.linkedin.com/in/danes17010"
         target="_blank"
         rel="noopener noreferrer"
-        className="mr-3 text-zinc-400 hover:text-[#69c7c7] drop-shadow-[1px_1px_0_#7836cf]"
-        aria-label="Linkedin"
+        className="text-zinc-400 hover:text-[#69c7c7] drop-shadow-[1px_1px_0_#7836cf]"
+        aria-label="LinkedIn"
       >
         <FaLinkedinIn className="h-7 w-7" />
       </a>
 
       <a
-        href="https://www.linkedin.com/in/danes17010/"
+        href="https://github.com/danes17010"
         target="_blank"
         rel="noopener noreferrer"
-        className="mx-3 text-zinc-400 hover:text-[#69c7c7] drop-shadow-[1px_1px_0_#7836cf]"
-        aria-label="Github"
+        className="text-zinc-400 hover:text-[#69c7c7] drop-shadow-[1px_1px_0_#7836cf]"
+        aria-label="GitHub"
       >
         <FaGithub className="h-7 w-7" />
-      </a>
-
-      <a
-        href="https://x.com/lautarodevelops/"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="mx-3 text-zinc-400 hover:text-[#69c7c7] drop-shadow-[1px_1px_0_#7836cf]"
-        aria-label="Twitter"
-      >
-        <FaXTwitter className="h-7 w-7" />
-      </a>
-
-      <a
-        href="https://www.instagram.com/lautarodevelops/"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="mx-3 text-zinc-400 hover:text-[#69c7c7] drop-shadow-[1px_1px_0_#7836cf]"
-        aria-label="Instagram"
-      >
-        <FaInstagram className="h-7 w-7" />
       </a>
     </div>
   );
