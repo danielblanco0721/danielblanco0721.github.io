354781f850fb1c1e29cfedb94524971d8635846d
danielblanco0721 <danielblanforero@gmail.com>
2026-01-14 18:12:56 -0500

    pto

--- src/data/projetsAboutData.js
+++ src/data/projetsAboutData.js
@@ -1,35 +1,27 @@
-import urbanStyle from "../assets/img-projects/urbanStyle.png";
-import imgNote from "../assets/img-projects/note-app.png";
+import bygnet from "../assets/img-projects/bygnet.jpeg";
+import delivery from "../assets/img-projects/delivery.png";
+import bygtrack from "../assets/img-projects/bygtrack.png";
 
 export const proyectos = [
   {
-    titulo: "E-commerce Urban Style",
-    descripcion: "Descripción breve del proyecto 1.",
-    imagen: urbanStyle.src,
-    tecnologias: ["React", "Tailwind", "Firebase"],
-    demo: "https://proyecto-final-rodriguez-sable.vercel.app/",
-    codigo: "https://github.com/Lautaro-R-collins/ProyectoFinal-Rodriguez.git",
-  },
-  {
-    titulo: "Tienda Artesana",
-    imagen:
-      "https://i.pinimg.com/736x/c9/70/bf/c970bfbc24489e39418b2c1b1f8f74ef.jpg",
-    tecnologias: ["React", "Node", "Express", "MongoDB"],
-    demo: "https://c21-05-ft-node-react-mirror-frontend-64qbwssi2-muyvisual.vercel.app/",
-    codigo: "https://github.com/No-Country-simulation/c21-05-ft-node-react",
-  },
-  {
-    titulo: "App organizacion",
-    imagen: imgNote.src,
-    tecnologias: ["React", "Tailwind", "Express", "MongoDB"],
-    demo: "https://front-react-todo.onrender.com",
-    codigo: "https://github.com/Lautaro-R-collins/Front-react-todo",
-  },
-  {
-    titulo: "API REST",
-    imagen: "https://miro.medium.com/1*47S4iWVwTicFkFL4-Z4uAA.png",
-    tecnologias: ["Express", "Node.js", "MongoDB", "Mongoose"],
-    demo: "https://back-express-todo.onrender.com",
-    codigo: "https://github.com/Lautaro-R-collins/Back-express-todo.git",
-  },
+      titulo: "Bygnet Solutions S.A.S",
+      imagen: bygnet.src,
+      tecnologias: ["React Native", "Laravel", "PostgreSQL"],
+      codigo: "https://github.com/danielblanco0721/bygnet.git",
+      categoria: "full-stack",
+    },
+    {
+      titulo: "Delivery",
+      imagen: delivery.src,
+      tecnologias: ["Vue.js", "Laravel", "PostgreSQL"],
+      demo: "https://delivery.housedev.co/login",
+      categoria: "fullstack",
+    },
+    {
+      titulo: "Bygtrack",
+      imagen: bygtrack.src,
+      tecnologias: ["Vue.js", "Laravel", "MySQL"],
+      demo: "https://bygtrack.hostingbygnet.com/bygtrack/public/",
+      categoria: "backend",
+    },
 ];
